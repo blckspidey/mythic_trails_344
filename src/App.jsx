56d845e7266068b8d3842b2686a404f6d6d38347
shantanu2104 <dukareshantanu2104@gmail.com>
2025-11-16 00:22:57 +0530

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Trails from "./pages/Trails";
+import TrailDetail from "./pages/TrailDetail";
+import Map from "./pages/Map";
+import Stories from "./pages/Stories";
+import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
+import BlogCreate from "./pages/BlogCreate";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <div className="min-h-screen flex flex-col">
+            <Navbar />
+            <main className="flex-1">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/trails" element={<Trails />} />
+                <Route path="/trail/:id" element={<TrailDetail />} />
+                <Route path="/map" element={<Map />} />
+                <Route path="/stories" element={<Stories />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/blog/:id" element={<BlogPost />} />
+                <Route path="/blog/create" element={<BlogCreate />} />
+                <Route path="/about" element={<About />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
